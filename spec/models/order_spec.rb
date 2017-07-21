cd4c93c65f6cebf6337e3c2bd7666992cf2e419e
Joe Kim <josephkim0224@gmail.com>
2017-07-21 13:51:45 -0400

    spec out initial order model

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+
+  describe 'validations' do
+
+    it 'requires a user' do
+      
+    end
+  end
+
+  describe 'relationships' do
+
+    it 'belongs to a user'
+  
+    it 'has many order items'
+
+    it 'has many items through order items'
+  end
+end
