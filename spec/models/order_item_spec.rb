eb16539af43e9fb759fadad6110fd9fb4a1d6859
Joe Kim <josephkim0224@gmail.com>
2017-07-21 14:05:15 -0400

    add passing validation tests for OrderItem

--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -4,7 +4,16 @@ RSpec.describe OrderItem, type: :model do
 
   describe 'validations' do
 
-    it 'requires a item, order, and quantity'
+    it 'requires a item, order, and quantity' do
+      order_item = OrderItem.new
+
+      expect(order_item.valid?).to eq(false)
+      expect(order_item.errors.full_messages).to eq([
+        "Order must exist",
+        "Item must exist",
+        "Quantity can't be blank"
+      ])
+    end
   end
 
   describe 'relationships' do
