99bde0825e511bba2f1bad28415d7cf0e707b12c
Joe Kim <josephkim0224@gmail.com>
2017-07-21 11:42:21 -0400

    update Cart relationship tests

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -16,10 +16,31 @@ RSpec.describe Cart, type: :model do
 
   describe 'relationships' do
 
-    it 'has many line items that are destroyed upon deltion of cart'
+    it 'has many line items that are destroyed upon deltion of cart' do
+      cart = create(:cart)
+      item = create(:item)
+      line_item = cart.line_items.create(quantity: 1, item: item)
 
-    it 'has many items through line items'
+      expect(cart.line_items.first.id).not_to equal(nil) 
 
-    it 'belongs to a user'
+      cart.destroy
+      line_item = LineItem.find_by(id: line_item.id)
+
+      expect(line_item).to eq(nil)
+    end
+
+    it 'has many items through line items' do
+      cart = create(:cart)
+      item = create(:item)
+      line_item = cart.line_items.create(quantity: 1, item: item)
+
+      expect(cart.items.count).to eq(1)
+    end
+
+    it 'belongs to a user' do
+      cart = create(:cart)
+
+      expect(cart.user.email).to eq("test@test.com")
+    end
   end
 end
