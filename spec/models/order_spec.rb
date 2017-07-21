b93fd7140d43c0dce2e444f6538170db49977164
Joe Kim <josephkim0224@gmail.com>
2017-07-21 13:55:35 -0400

    add passing order model validation spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,7 +5,12 @@ RSpec.describe Order, type: :model do
   describe 'validations' do
 
     it 'requires a user' do
-      
+      order = Order.new
+
+      expect(order.valid?).to eq(false)
+      expect(order.errors.full_messages).to eq([
+        "User must exist"
+      ])
     end
   end
 
