1ad859962c92c7e4ce776f99c17b960ddbd5e47f
Joe Kim <josephkim0224@gmail.com>
2017-07-21 10:59:48 -0400

    finish email and password presence test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,15 @@ RSpec.describe User, type: :model do
 
   describe 'validation' do
 
-    it 'requires an email and password upon creation'
+    it 'requires an email and password upon creation' do
+      user = build(:user, email: nil, password: nil)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([
+        "Password can't be blank",
+        "Email can't be blank"
+      ])
+    end
 
     it 'requres that an email is unique'
   end
