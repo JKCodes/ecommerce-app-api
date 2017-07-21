c43e17389077cdae9c87b71eccd3748d4adee8ac
Joe Kim <josephkim0224@gmail.com>
2017-07-21 12:55:51 -0400

    update LineItem migration

--- db/migrate/20170721165443_create_line_items.rb
+++ db/migrate/20170721165443_create_line_items.rb
@@ -3,7 +3,7 @@ class CreateLineItems < ActiveRecord::Migration[5.1]
     create_table :line_items do |t|
       t.references :cart, foreign_key: true
       t.references :item, foreign_key: true
-      t.integer :quantity
+      t.integer :quantity, default: 1, null: false
 
       t.timestamps
     end
