81f3f8f3520c4bad8c185c3855c05b4121ba5ca1
Joe Kim <josephkim0224@gmail.com>
2017-07-21 13:33:33 -0400

    add OrderItem migration, fix Item migration, and add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721171942) do
+ActiveRecord::Schema.define(version: 20170721173231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170721171942) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.integer "inventory"
-    t.decimal "price"
+    t.decimal "price", precision: 8, scale: 2
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 20170721171942) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["item_id"], name: "index_line_items_on_item_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "order_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -67,5 +77,7 @@ ActiveRecord::Schema.define(version: 20170721171942) do
   add_foreign_key "items", "categories"
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "items"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
