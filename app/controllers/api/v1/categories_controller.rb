8bf337af84ca4bed6b24e4beeb8bfd6096dda3cc
Joe Kim <josephkim0224@gmail.com>
2017-08-18 07:15:04 -0400

    add index, show, and category_params

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,20 @@
 class Api::V1::CategoriesController < ApplicationController
-  
+
+  def index
+    render json: Category.All
+  end
+
+  def create
+
+  end
+
+  def show
+    render json: Category.find_by(id: params[:id])
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:title)
+    end
 end
