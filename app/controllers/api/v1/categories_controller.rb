35a5e914d962aaf1568ac812f7deb8e18a4a1878
Joe Kim <josephkim0224@gmail.com>
2017-08-18 07:21:35 -0400

    add set_category before_action and show action

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,18 +1,32 @@
 class Api::V1::CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
 
   def index
     render json: Category.All
   end
 
   def create
-
+    category = Category.new(category_params)
+    if category.save
+      render json: category
+    else
+      render json: { error: category.errors }, status: 400
+    end
   end
 
   def show
-    render json: Category.find_by(id: params[:id])
+    render json: @category
+  end
+
+  def update
+
   end
 
   private
+
+    def set_category
+      @category = Category.find_by(id: params[:id])
+    end
 
     def category_params
       params.require(:category).permit(:title)
