28375483f8db11b5b31cfb3687db95cda74d17d1
Joe Kim <josephkim0224@gmail.com>
2017-08-18 07:04:24 -0400

    add routes for categories and items

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
 
       # /api/v1/carts
       resources :carts, only: [:show, :update]
+
+      # /api/v1/categories
+      resources :categories, except: [:new, :edit]
+
+      # /api/v1/items
+      resources :items, except: [:new, :edit]
     end
   end
 end
