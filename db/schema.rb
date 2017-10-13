fd9ad5e9307f692ef3fc2fe8f5997a75115b1e56
Joe Kim <josephkim0224@gmail.com>
2017-10-13 13:23:25 -0400

    update schema to include the newly added columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721173231) do
+ActiveRecord::Schema.define(version: 20171013172217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20170721173231) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img_url"
+    t.string "description"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
@@ -71,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170721173231) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
   add_foreign_key "carts", "users"
